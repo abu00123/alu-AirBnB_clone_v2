fe3f145ba8895681d4c784a124e6485c790fffd2
abu00123 <a.garang@alustudent.com>
2025-02-23 04:19:02 -0800

    work

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,25 @@
-from fabric.api import local
 from datetime import datetime
 import os
+import subprocess
 
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder.
+    Creates a .tgz archive from the contents of the web_static folder.
     """
+    # Create the versions directory if it does not exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    
+    # Generate the archive name using the current timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{timestamp}.tgz"
+    
+    # Create the archive using tar
+    print(f"Packing web_static to {archive_name}")
     try:
-        # Create the versions directory if it doesn't exist
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-
-        # Generate the archive name using the current date and time
-        now = datetime.now()
-        archive_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-
-        # Create the archive using the tar command
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-
-        # Return the path to the archive if successful
-        return "versions/{}".format(archive_name)
-    except Exception as e:
-        # Return None if any error occurs
+        result = subprocess.run(["tar", "-cvzf", archive_name, "web_static"], check=True)
+    except subprocess.CalledProcessError:
         return None
+    
+    return archive_name
+
